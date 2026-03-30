1b187a479f50d230e5e85965e59e7de3d10cd0ed
Maximilian Miller <67687590+maxtmiller@users.noreply.github.com>
2026-03-29 20:52:41 -0400

    Update validator.ts

--- types/validator.ts
+++ types/validator.ts
@@ -36,10 +36,10 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
 }
 
 
-// Validate ../../src/app/page.tsx
+// Validate ../app/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
-  const handler = {} as typeof import("../../src/app/page.js")
+  const handler = {} as typeof import("../app/page.tsx")
   type __Check = __IsExpected<typeof handler>
   // @ts-ignore
   type __Unused = __Check
